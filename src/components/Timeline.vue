7682e7670c7d848e4edeebdd9488b15ae96648e5
krummhaus <jiri.kalina.jr@gmail.com>
2024-11-19 12:10:09 +0100

    lesson13 end

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
 // every variable defined here will be automaticaly avilible in <template>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { Post, today, thisWeek, thisMonth } from "../posts"
+import { DateTime } from "luxon";
 
-const periods = ["Today", "This week", "This month"];
+// Own type the long way
+// type Period = "Today" | "This week" | "This month"
+// const periods: Period[] = ["Today", "This week", "This month"];
 
-const selectedPeriod = ref("Today");
+// Own type short way
+const periods = ["Today", "This week", "This month"] as const;
+type Period = typeof periods[number];
 
-function selectPeriod (period: string) {
+const selectedPeriod = ref<Period>("Today");
+
+// We can use '.reduce' instead of '.map' + '.filter'
+const posts = computed(() => {
+  return [today, thisWeek, thisMonth]
+  .map(post => {
+    return {
+      ...post,
+      created: DateTime.fromISO(post.created)
+    };
+  })
+  .filter(post => {
+    if (selectedPeriod.value === "Today") {
+      return post.created >= DateTime.now().minus({ day: 1 })
+    }
+    if (selectedPeriod.value === "This week") {
+      return post.created >= DateTime.now().minus({ week: 1 })
+    }
+    return post 
+  })
+})
+
+function selectPeriod (period: Period) {
   console.log(period);
   selectedPeriod.value = period;
 }
@@ -25,5 +53,10 @@ function selectPeriod (period: string) {
         {{ period }}
       </a>
     </span>
+
+    <a v-for="post of posts" :key="post.id" class="panel-block">
+      <a>{{  post.title }}</a>
+      <div>{{  post.created.toFormat("d. M. y") }}</div>
+    </a>
   </nav>
 </template>
